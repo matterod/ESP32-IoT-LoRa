845cb7501ed7873c8bb0391b2758001c8d0550ad
Mateo Rodriguez <139497031+matterod@users.noreply.github.com>
2024-10-02 10:47:31 -0300

    Update CodigoCalentador.ino

--- CodigoCalentador.ino
+++ CodigoCalentador.ino
@@ -3,15 +3,15 @@
 #include <DHT.h>
 #include <RCSwitch.h>
 
-#define WIFI_SSID "Starlink Yehuin"
-#define WIFI_PASSWORD "solom854"
+#define WIFI_SSID "SSID del WIFI al que se va a conectar el ESP32"
+#define WIFI_PASSWORD "Contra WIFI"
 
 #define DHTPIN 22
 #define DHTTYPE DHT11
 #define SEND_PIN 12
 
-#define FIREBASE_HOST "esp32rinconada-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "77P1aUlOPjRGDOGVT6yzVpvOvJmRbFXcm1v5FzKB"
+#define FIREBASE_HOST "Tu Firebase Host"
+#define FIREBASE_AUTH "El ID del mail del cliente"
 
 FirebaseData firebaseData;
 FirebaseConfig firebaseConfig;
@@ -20,7 +20,7 @@ FirebaseAuth firebaseAuth;
 DHT dht(DHTPIN, DHTTYPE);
 RCSwitch mySwitch = RCSwitch();
 
-const char* deviceID = "dFvwpoPyysZDF9ZVuBRfeKqpmGQ2";
+const char* deviceID = "el ID del cliente";
 
 unsigned long previousMillis = 0;
 unsigned long previousReconnectMillis = 0;
@@ -29,9 +29,9 @@ const long reconnectInterval = 20000; // Intentar reconectar Firebase cada 30 se
 bool firebaseConnected = true;
 
 int rfProtocol = 1;
-unsigned long rfCodeOn = 734728624; //CODIGO ENCENDIDO
-unsigned long rfCodeOff = 734725384;  //CODIGO APAGADO
-
+unsigned long rfCodeOn = //El codigo se tiene que obtener de un receptor RF; //CODIGO ENCENDIDO
+unsigned long rfCodeOff = //El codigo se tiene que obtener de un receptor RF;  //CODIGO APAGADO
+//Codigo de Receptor en el Repositorio.
 void setup() {
   Serial.begin(115200);
   dht.begin();
@@ -91,7 +91,7 @@ void loop() {
   }
 
   if (Firebase.ready()) {
-    String path = String("/users/") + String(deviceID) + "/LedStatus";
+    String path = String("/users/") + String(deviceID) + "/LedStatus"; //Importante seguir esta estructura en Firebase.
     if (Firebase.getString(firebaseData, path)) {
       String ledStatus = firebaseData.stringData();
       
